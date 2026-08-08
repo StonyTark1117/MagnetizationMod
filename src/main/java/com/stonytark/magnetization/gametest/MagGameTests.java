eb0ba0af9da8fa8ee61a8014b7dfef7d83556666
StonyTark1117 <braydonsanders2001@gmail.com>
2026-08-08 12:34:03 -0700

    Fix sky-limit gas GameTest coordinates

--- src/main/java/com/stonytark/magnetization/gametest/MagGameTests.java
+++ src/main/java/com/stonytark/magnetization/gametest/MagGameTests.java
@@ -538,16 +538,18 @@ public final class MagGameTests {
 
     @GameTest(template = EMPTY_TEMPLATE, timeoutTicks = 100)
     public static void fusionGasesDespawnAtSkyLimit(final GameTestHelper helper) {
-        final BlockPos skyLimit = new BlockPos(1, helper.getLevel().getMaxBuildHeight() - 1, 1);
+        final BlockPos structureOrigin = helper.absolutePos(new BlockPos(1, 1, 1));
+        final BlockPos skyLimit = new BlockPos(structureOrigin.getX(),
+                helper.getLevel().getMaxBuildHeight() - 1, structureOrigin.getZ());
         final net.minecraft.world.level.block.Block[] gases = {
                 MagBlocks.HYDROGEN_BLOCK.get(), MagBlocks.TRITIUM_BLOCK.get(), MagBlocks.HELIUM_3_BLOCK.get()
         };
         for (int i = 0; i < gases.length; i++) {
-            helper.setBlock(skyLimit.offset(i, 0, 0), gases[i]);
+            helper.getLevel().setBlock(skyLimit.offset(i, 0, 0), gases[i].defaultBlockState(), 3);
         }
         helper.runAfterDelay(20L, () -> {
             for (int i = 0; i < gases.length; i++) {
-                helper.assertTrue(helper.getBlockState(skyLimit.offset(i, 0, 0)).isAir(),
+                helper.assertTrue(helper.getLevel().getBlockState(skyLimit.offset(i, 0, 0)).isAir(),
                         "Gas should despawn at the sky limit: " + gases[i]);
             }
             helper.succeed();
